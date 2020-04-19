d1a7e27b4c527e4bddb917b0933ca013afca4818
Michal <mmajchrzak@DESKTOP-J05DKRU.localdomain>
2020-04-19 23:55:05 +0200

    Another test commit, just to remember

--- hello.c
+++ hello.c
@@ -5,4 +5,6 @@ int main() {
   printf("Hello world!\n");
   printf("This line is added locally\n");
   printf("Another line added by someone else\n");
+  printf("Yet another line, just to make change\n");
+
 }
